14c632a43935a96c2b5f66d8926f00a65f1712fe
luisrubio <luisrubio8@gmail.com>
2019-04-11 14:06:27 -0500

    feature: Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const tweets = require("./routes/api/tweets");
 const app = express();
@@ -28,5 +29,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/tweets", tweets);
+
+// static assets
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(client / build));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Tweeter App listening on port ${port}!`));
