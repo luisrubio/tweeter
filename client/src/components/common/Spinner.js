2ee02fb00a41ef463e4323b1965f5fa6206520e0
luisrubio <luisrubio8@gmail.com>
2019-04-13 20:06:14 -0500

    feature: Add spinner and css tweaks

---
+++ client/src/components/common/Spinner.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="has-text-centered">
+      <img
+        className="spinner"
+        src="https://loading.io/spinners/ellipsis/lg.discuss-ellipsis-preloader.gif"
+      />
+    </div>
+  );
+};
+
+export default Spinner;
